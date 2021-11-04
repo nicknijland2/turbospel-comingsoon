5f147af828dd519e2b269ee6ff41fff0ee396294
Nick Nijland <n.a.nijland.2005@gmail.com>
2021-11-04 14:22:40 +0100

    countdown 380 days

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
     const cfg = {
 
         // Countdown Timer Final Date
-        finalDate : 'April 30, 2022 12:00:00',
+        finalDate : '19 november, 2022 12:00:00',
         // MailChimp URL
         mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
 
